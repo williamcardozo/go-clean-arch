b05f43738aa0c30245c92b54643faa8ef5454598
William Goulardt Cardozo <wgoulardtcardozo@gmail.com>
2026-03-16 21:41:08 -0300

    quick refactoring

--- internal/usecase/create_order.go
+++ internal/usecase/create_order.go
@@ -4,19 +4,6 @@ import (
 	"github.com/williamcardozo/go-clean-arch/internal/entity"
 )
 
-type OrderInputDTO struct {
-	ID    string  `json:"id"`
-	Price float64 `json:"price"`
-	Tax   float64 `json:"tax"`
-}
-
-type OrderOutputDTO struct {
-	ID         string  `json:"id"`
-	Price      float64 `json:"price"`
-	Tax        float64 `json:"tax"`
-	FinalPrice float64 `json:"final_price"`
-}
-
 type CreateOrderUseCase struct {
 	OrderRepository entity.OrderRepositoryInterface
 }
